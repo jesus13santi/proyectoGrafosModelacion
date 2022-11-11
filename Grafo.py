50e1fab48b0d78d39615143cab35a64118c63b3a
jesus13santi <jesus13santi@gmail.com>
2022-11-11 11:15:12 -0400

    first commit

---
+++ Grafo.py
@@ -0,0 +1,368 @@
+import math
+
+
+class Grafo:
+
+    def __init__(self):
+        self.vertices = []
+        self.matriz = [[None]*0 for i in range(0)]
+
+    def imprimir_matriz(self, m, texto):
+        cadena = ""
+
+        for c in range(len(m)):
+            cadena += "|" + str(self.vertices[c])
+
+        cadena += "\n " + (" -" * len(m))
+
+        for f in range(len(m)):
+            cadena += "\n" + str(self.vertices[f]) + " |"
+            for c in range(len(m)):
+                if texto:
+                    cadena += "\t" + str(m[f][c])
+                else:
+                    if f == c and (m[f][c] is None or m[f][c] == 0):
+                        cadena += " " + "\\"
+                    else:
+                        if m[f][c] is None or math.isinf(m[f][c]):
+                            cadena += " " + "X"
+                        else:
+                            cadena += " " + str(m[f][c])
+
+        cadena += "\n"
+        print(cadena)
+
+    @staticmethod
+    def contenido_en(lista, k):
+        if lista.count(k) == 0:
+            return False
+        return True
+
+    def esta_en_vertices(self, v):
+        if self.vertices.count(v) == 0:
+            return False
+        return True
+
+    def agregar_vertices(self, v):
+        if self.esta_en_vertices(v):
+            return False
+        # Si no esta contenido.
+        self.vertices.append(v)
+
+        # Se redimenciona la matriz de adyacencia.
+        # Para preparalarla para agregar más Aristas.
+        filas = columnas = len(self.matriz)
+        matriz_aux = [[None] * (filas+1) for i in range(columnas+1)]
+
+        # Se recorre la matriz y se copia su contenido dentro de la matriz más grande.
+        for f in range(filas):
+            for c in range(columnas):
+                matriz_aux[f][c] = self.matriz[f][c]
+
+        # Se iguala la matriz a la matriz más grande.
+        self.matriz = matriz_aux
+        return True
+
+    def agregar_arista(self, inicio, fin, valor, dirijida):
+        if not (self.esta_en_vertices(inicio)) or not (self.esta_en_vertices(fin)):
+            return False
+        # Si estan contenidos en la lista de vertices.
+        self.matriz[self.vertices.index(
+            inicio)][self.vertices.index(fin)] = valor
+
+        # Si la arista entrante no es dirijida.
+        # Instancio una Arista en sentido contrario de Fin a Inicio.
+        if not dirijida:
+            self.matriz[self.vertices.index(
+                fin)][self.vertices.index(inicio)] = valor
+        return True
+
+    def recorrido_anchura(self, inicio):
+        if not self.esta_en_vertices(inicio):
+            return None
+
+        recorrido = []
+        cola = deque([inicio])
+
+        while len(cola) > 0:
+            v_aux = cola.popleft()
+            recorrido.append(v_aux)
+
+            for i in range(len(self.matriz)):
+                if self.matriz[self.vertices.index(v_aux)][i] is not None:
+                    v_candidato = self.vertices[i]
+                    if not self.contenido_en(recorrido, v_candidato) and not self.contenido_en(cola, v_candidato):
+                        cola.append(v_candidato)
+
+        return recorrido
+
+    def recorrido_profundidad(self, inicio):
+        if not self.esta_en_vertices(inicio):
+            return None
+
+        recorrido = []
+        pila = [inicio]
+
+        while len(pila) > 0:
+            v_aux = pila.pop()
+
+            if not self.contenido_en(recorrido, v_aux):
+                recorrido.append(v_aux)
+
+            condicion = True
+
+            for i in range(len(self.matriz)):
+                if self.matriz[self.vertices.index(v_aux)][i] is not None:
+                    v_candidato = self.vertices[i]
+
+                    if not self.contenido_en(recorrido, v_candidato) and condicion:
+                        # Es como un Break.
+                        condicion = False
+
+                        pila.append(v_aux)
+                        pila.append(v_candidato)
+
+        return recorrido
+
+    def obtener_sucesores(self, v):
+        pos_vertice = self.vertices.index(v)
+
+        list_sucesores = []
+
+        for i in range(len(self.matriz)):
+            if self.matriz[pos_vertice][i] is not None:
+                list_sucesores.append(self.vertices[i])
+
+        return list_sucesores
+
+    # Aciclico.
+    def camino(self, k, v2):
+        # Con ciclos.
+        return self.__camino(k, v2, [])
+
+    def __camino(self, k, v2, visitados):
+        if k == v2:
+            return True
+
+        visitados.append(k)
+        sucesores = self.obtener_sucesores(k)
+
+        for vertice in sucesores:
+            if not self.contenido_en(visitados, vertice):
+                if self.__camino(vertice, v2, visitados):
+                    return True
+
+        return False
+
+    # Es un booleano que verifica si todos los nodos estan marcados
+    def allMarked(self, marcados):
+        for i in range(marcados.__len__()):
+            if not marcados[i]:
+                return False
+        return True
+
+    # Se utiliza para verificar el menor de los no marcados en un vector
+    def MinValueNotMarqued(self, pesos, marcados):
+        # Inicializamos min en un numero infinito 999
+        min = 999
+
+        # Se inicializa pos en una posicion imposible para guardar el valor luego
+        pos = -1
+
+        # Se recorre el vector de pesos
+        for i in range(pesos.__len__()):
+
+            # Si se encuentra un vertice menor que la variable auxiliar y que no este
+            # Ligado a un vertice marcado se remplaza la posicion y se guarda el valor
+            if pesos[i] < min and not marcados[i]:
+                min = pesos[i]
+                pos = i
+        return pos
+
+    # Se hace lo mismo que para pesosDijikstra con la diferencia que se retorna
+    # Un vector de recorridos
+    def RecorridoDijikstra(self, inicio):
+        pesos = []
+        recorridos = []
+        marcados = []
+
+        pesos = [999 for i in range(self.matriz.__len__())]
+        marcados = [False for i in range(self.matriz.__len__())]
+        recorridos = [0 for i in range(self.matriz.__len__())]
+        pesos[self.vertices.index(inicio)] = 0
+
+        while (not self.allMarked(marcados)):
+            aux = self.MinValueNotMarqued(pesos, marcados)
+            if aux == -1:
+                break
+            marcados[aux] = True
+            for i in range(self.matriz.__len__()):
+                if self.matriz[aux][i] != None:
+                    p = self.matriz[aux][i]
+                    if p + pesos[aux] < pesos[i]:
+                        pesos[i] = p + pesos[aux]
+                        recorridos[i] = self.vertices[aux]
+        # print(recorridos)
+        return recorridos
+
+    # Se utiliza para verificar todos los pesos de los nodos para llegar a inicio
+    def PesosDijikstra(self, inicio):
+        pesos = []
+        recorridos = []
+        marcados = []
+
+        # Se inicializan todos los nodos con un valor infinito en este caso 999
+        pesos = [999 for i in range(self.matriz.__len__())]
+        # Se crea una lista que contieen los espacios del tamano de la matriz con False
+        # Verfica que los nodos ya esten recorridos
+        marcados = [False for i in range(self.matriz.__len__())]
+
+        # Colocamos una lista con 0 del tamano de la matriz original
+        recorridos = [0 for i in range(self.matriz.__len__())]
+
+        # Inicializamos la posicion del vertice inicial denro del vector de pesos
+        # Con un peso de 0
+        pesos[self.vertices.index(inicio)] = 0
+
+        # Mientras que se encuentren vertices sin marcar dentro del vector Marcados
+        # Se ejecuta
+        while (not self.allMarked(marcados)):
+            # Se inicializa una variable auxiliar con el menor valor de los vertices
+            # No marcados que se encuentren dentro del vector pesos
+            aux = self.MinValueNotMarqued(pesos, marcados)
+
+            # Si no exite el valor termina el proceso
+            if aux == -1:
+                break
+            # Se marca el vertice auxiliar dentro del vector de vertices marcados
+            marcados[aux] = True
+
+            # Se recorre la matriz de adyacencia
+            # Buscando los vertices adyacentes a la variable auxiliar
+            for i in range(self.matriz.__len__()):
+                # Si existe un vertice adyacente al nodo auxiliar
+                if self.matriz[aux][i] != None:
+                    # Se intancia una variable p para guardar el peso
+                    p = self.matriz[aux][i]
+
+                    # Si el peso del vertice adyacente mas el peso del recorrido realizado
+                    # es menor al peso del recorrido anterior dentro del vecto de pesos
+                    # Se reemplaza
+                    if p + pesos[aux] < pesos[i]:
+                        pesos[i] = p + pesos[aux]
+                        recorridos[i] = self.vertices[aux]
+        # print(pesos)
+        return pesos
+
+    # Se hace para tomar el camino mas corto (Retorna una lista)
+    def DijikstraCamino(self, inicio, fin):
+        # Se obtienen todos los caminos de Dijkstra desde el nodo inicio
+        recorridos = self.RecorridoDijikstra(inicio)
+
+        # Se inicializa la lista de salidas con el nodo final
+        salida = [fin]
+
+        # Verificamos que el nodo inicio sea diferentre que el ultimo elemento
+        # De la lista de salida y agregamos a la lista de salida
+        while inicio != salida[salida.__len__()-1]:
+            salida.append(
+                recorridos[self.vertices.index(salida[salida.__len__()-1])])
+
+        # Revertimos la lista para que quede en su forma original
+        recorridos = list(reversed(salida))
+
+        # Se retorna una lista con los recorridos
+        return recorridos
+        # while inicio != list(salida)[salida.__len__()-1]:
+
+        #     salida += recorridos[self.vertices.index(
+        #         list(salida)[salida.__len__()-1])]
+        # salida = ''.join(reversed(salida))
+        # recorridos = list(salida)
+        # return recorridos
+        # print(recorridos)
+
+    # SE UTILIZA PARA CREAR EL GRAFO
+    def DijikstraGrafo(self, inicio, fin):
+        # Si la lista de vertices no contiene el vertice inicio o el vertice final
+        # No se puede recorrer
+        if self.vertices.index(inicio) == -1 or self.vertices.index(fin) == -1:
+            return None
+        # Se instancia un grafo para guardar la informacion
+        g = Grafo()
+
+        # Se encuentra el camino (Retorna una lista)
+        recorrido = self.DijikstraCamino(inicio, fin)
+        # print(recorrido)
+
+        # Retorna la lista de pesos de los grafos
+        pesos = self.PesosDijikstra(inicio)
+
+        # Retorna el peso del grafo total
+        pesosCamino = pesos[self.vertices.index(fin)]
+
+        # Se agregan los vertices al nuevo grafo
+        for i in recorrido:
+
+            g.agregar_vertices(i)
+
+        # Se agregan las aristas de los vertices del nuevo grafo
+        # Con el peso en cada vertice desde el inicio hasta ese vertice
+        for i in range(recorrido.__len__()-1):
+            pesosAux = self.PesosDijikstra(recorrido[i])
+            pesosCaminoAux = pesosAux[self.vertices.index(recorrido[i+1])]
+            g.agregar_arista(
+                recorrido[i], recorrido[i+1], pesosCaminoAux, True)
+
+        return g
+
+    # Se hace doble Dijkstra y se modifican las matrices
+    def CaminoCortoJavierAndreina(self, inicioJavier, inicioAndreina, fin):
+        g = self
+
+        gd = g.DijikstraGrafo(inicioJavier, fin)
+        gdPesos = gd.PesosDijikstra(inicioJavier)
+        # print(gPesos)
+        # print(gd.imprimir_matriz(gd.matriz, False))
+        print("Camino mas corto Javier:")
+        print(gd.vertices)
+        print("El camino mas corto para Javier es de" +
+              " " + str(gdPesos[gdPesos.__len__()-1]))
+
+        # Se hace para cololar los nodos ya visitados por Javier
+        # Que no sean visitados por Andreina
+        for i in gd.vertices:
+            # print(self.matriz[self.vertices.index(i)])
+            for a in range(g.matriz[g.vertices.index(i)].__len__()):
+                if g.matriz[g.vertices.index(i)][a] != None:
+                    g.matriz[g.vertices.index(i)][a] = 999
+
+            for a in g.matriz:
+                if (a[0] != None):
+                    a[0] = 99
+        print("-------------------------")
+
+        # Matriz con los pesos de Andreina
+        for i in range(g.matriz.__len__()):
+            for a in range(g.matriz[i].__len__()):
+                if g.matriz[i][a] != None and g.matriz[i][a] != 999:
+                    g.matriz[i][a] += 2
+
+        print("Camino mas corto Andreina: ")
+        gd2 = g.DijikstraGrafo(inicioAndreina, fin)
+        pesosg2 = gd2.PesosDijikstra(inicioAndreina)
+        print(gd2.vertices)
+        print("El camino mas corto para Andreina es de " +
+              " "+str(pesosg2[pesosg2.__len__()-1]))
+        # print(gd2.PesosDijikstra(inicioAndreina))
+
+        if gdPesos[gdPesos.__len__()-1] > pesosg2[pesosg2.__len__()-1]:
+            resta = gdPesos[gdPesos.__len__()-1] - pesosg2[pesosg2.__len__()-1]
+            print("Para que puedan llegar iguales Andreina debe salir " +
+                  str(resta) + " minutos antes que Javier")
+        elif gdPesos[gdPesos.__len__()-1] < pesosg2[pesosg2.__len__()-1]:
+            resta = pesosg2[pesosg2.__len__()-1] - gdPesos[gdPesos.__len__()-1]
+            print("Para que puedan llegar iguales, Javier debe salir " +
+                  str(resta) + " minutos antes que Andreina")
+        else:
+            print("Para que puedan llegar iguales pueden salir al mismo tiempo")
---
+++ Main.py
@@ -0,0 +1,161 @@
+import math
+import copy
+from Grafo import Grafo
+
+
+g = Grafo()
+gInicial = Grafo()
+
+g.agregar_vertices("50-11")
+g.agregar_vertices("50-12")
+g.agregar_vertices("50-13")
+g.agregar_vertices("50-14")
+g.agregar_vertices("50-15")
+
+g.agregar_vertices("51-11")
+g.agregar_vertices("51-12")
+g.agregar_vertices("51-13")
+g.agregar_vertices("51-14")
+g.agregar_vertices("51-15")
+
+g.agregar_vertices("52-11")
+g.agregar_vertices("52-12")
+g.agregar_vertices("52-13")
+g.agregar_vertices("52-14")
+g.agregar_vertices("52-15")
+
+g.agregar_vertices("53-11")
+g.agregar_vertices("53-12")
+g.agregar_vertices("53-13")
+g.agregar_vertices("53-14")
+g.agregar_vertices("53-15")
+
+g.agregar_vertices("54-11")
+g.agregar_vertices("54-12")
+g.agregar_vertices("54-13")
+g.agregar_vertices("54-14")
+g.agregar_vertices("54-15")
+
+g.agregar_vertices("51-11")
+g.agregar_vertices("51-12")
+g.agregar_vertices("51-13")
+g.agregar_vertices("51-14")
+g.agregar_vertices("51-15")
+
+g.agregar_vertices("52-11")
+g.agregar_vertices("52-12")
+g.agregar_vertices("52-13")
+g.agregar_vertices("52-14")
+g.agregar_vertices("52-15")
+
+g.agregar_vertices("53-11")
+g.agregar_vertices("53-12")
+g.agregar_vertices("53-13")
+g.agregar_vertices("53-14")
+g.agregar_vertices("53-15")
+
+g.agregar_vertices("54-11")
+g.agregar_vertices("54-12")
+g.agregar_vertices("54-13")
+g.agregar_vertices("54-14")
+g.agregar_vertices("54-15")
+
+# m_floyd, m_warshall = g.floyd_warshall()
+
+# No dirigido.
+g.agregar_arista("50-11", "50-12", 5, False)
+g.agregar_arista("50-12", "50-13", 5, False)
+g.agregar_arista("50-13", "50-14", 5, False)
+g.agregar_arista("50-14", "50-15", 5, False)
+
+g.agregar_arista("51-11", "51-12", 10, False)
+g.agregar_arista("51-12", "51-13", 10, False)
+g.agregar_arista("51-13", "51-14", 10, False)
+g.agregar_arista("51-14", "51-15", 10, False)
+
+g.agregar_arista("52-11", "52-12", 5, False)
+g.agregar_arista("52-12", "52-13", 5, False)
+g.agregar_arista("52-13", "52-14", 5, False)
+g.agregar_arista("52-14", "52-15", 5, False)
+
+g.agregar_arista("53-11", "53-12", 5, False)
+g.agregar_arista("53-12", "53-13", 5, False)
+g.agregar_arista("53-13", "53-14", 5, False)
+g.agregar_arista("53-14", "53-15", 5, False)
+
+g.agregar_arista("54-11", "54-12", 5, False)
+g.agregar_arista("54-12", "54-13", 5, False)
+g.agregar_arista("54-13", "54-14", 5, False)
+g.agregar_arista("54-14", "54-15", 5, False)
+
+g.agregar_arista("50-11", "51-11", 5, False)
+g.agregar_arista("51-11", "52-11", 5, False)
+g.agregar_arista("52-11", "53-11", 5, False)
+g.agregar_arista("53-11", "54-11", 5, False)
+
+g.agregar_arista("50-12", "51-12", 7, False)
+g.agregar_arista("51-12", "52-12", 7, False)
+g.agregar_arista("52-12", "53-12", 7, False)
+g.agregar_arista("53-12", "54-12", 7, False)
+
+g.agregar_arista("50-13", "51-13", 7, False)
+g.agregar_arista("51-13", "52-13", 7, False)
+g.agregar_arista("52-13", "53-13", 7, False)
+g.agregar_arista("53-13", "54-13", 7, False)
+
+g.agregar_arista("50-14", "51-14", 7, False)
+g.agregar_arista("51-14", "52-14", 7, False)
+g.agregar_arista("52-14", "53-14", 7, False)
+g.agregar_arista("53-14", "54-14", 7, False)
+
+g.agregar_arista("50-15", "51-15", 5, False)
+g.agregar_arista("51-15", "52-15", 5, False)
+g.agregar_arista("52-15", "53-15", 5, False)
+g.agregar_arista("53-15", "54-15", 5, False)
+
+gInicial = copy.deepcopy(g)
+# g.agregar_vertices("A")
+# g.agregar_vertices("B")
+# g.agregar_vertices("C")
+# g.agregar_vertices("D")
+# g.agregar_vertices("E")
+# g.agregar_vertices("F")
+
+# g.agregar_vertices("G")
+# g.agregar_vertices("H")
+# g.agregar_vertices("I")
+# g.agregar_vertices("J")
+# g.agregar_vertices("K")
+
+# g.agregar_arista("A", "B", 5, False)
+# g.agregar_arista("A", "D", 4, False)
+# g.agregar_arista("A", "E", 2, False)
+# g.agregar_arista("B", "C", 1, False)
+# g.agregar_arista("B", "E", 1, False)
+# g.agregar_arista("C", "F", 5, False)
+# g.agregar_arista("D", "C", 3, False)
+# g.agregar_arista("D", "E", 3, False)
+# g.agregar_arista("D", "F", 4, False)
+# g.agregar_arista("E", "F", 8, False)
+
+# g.agregar_arista("F", "G", 3, False)
+# g.agregar_arista("F", "H", 2, False)
+# g.agregar_arista("G", "H", 5, False)
+# g.agregar_arista("G", "I", 1, False)
+# g.agregar_arista("G", "J", 4, False)
+# g.agregar_arista("I", "J", 2, False)
+# g.agregar_arista("J", "K", 5, False)
+# g.agregar_arista("I", "K", 4, False)
+
+
+g.CaminoCortoJavierAndreina("54-14", "52-13", "54-11")
+
+
+# Encontrar la matriz de Andreina
+# for i in range(gInicial.matriz.__len__()):
+#     for a in range(gInicial.matriz[i].__len__()):
+#         if gInicial.matriz[i][a] != None:
+#             gInicial.matriz[i][a] += 2
+
+
+# gInicial.imprimir_matriz(gInicial.matriz, False)
